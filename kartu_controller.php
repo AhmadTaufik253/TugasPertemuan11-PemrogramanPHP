63c378f2f9baa4a6c282d731392041f12ef7a16b
AhmadTaufik253 <ahmadtaufik2503@gmail.com>
2023-05-09 23:00:39 +0700

    tugas pertemuan 11

---
+++ kartu_controller.php
@@ -0,0 +1,34 @@
+<?php
+include_once 'koneksi.php';
+include_once 'models/Kartu.php';
+
+// menangkap request form
+$kode = $_POST['kode'];
+$nama = $_POST['nama'];
+$diskon = $_POST['diskon'];
+$iuran = $_POST['iuran'];
+
+// menangkap form diatas dan dijadikan array
+$data = [
+    $kode,
+    $nama,
+    $diskon,
+    $iuran,
+];
+
+$model = new Kartu();
+$tombol = $_REQUEST['proses'];
+switch ($tombol) {
+    case 'simpan':$model->simpan($data);
+    break;
+    case 'ubah' :
+        $data[] = $_POST['idx']; $model->ubah($data);
+    break;
+    default:
+        header('Location:index.php?url=kartu');
+     break;
+}
+header('Location:index.php?url=kartu');
+
+
+?>
---
+++ models/Pelanggan.php
@@ -0,0 +1,32 @@
+<?php
+class Pelanggan{
+    private $koneksi;
+    public function __construct(){
+        global $dbh;
+        $this->koneksi = $dbh;
+    }
+    public function Pelanggan(){
+        $sql = "SELECT pelanggan.*, kartu.nama AS kartu FROM pelanggan JOIN kartu ON pelanggan.kartu_id = kartu.id";
+        $ps = $this->koneksi->prepare($sql);
+        $ps->execute();
+        $rs = $ps->fetchAll();
+        return $rs;
+    }
+
+    public function getPelanggan($id){
+        $sql = "SELECT pelanggan.*, kartu.nama AS kartu FROM pelanggan JOIN kartu ON pelanggan.kartu_id = kartu.id WHERE pelanggan.id = ?";
+        $ps = $this->koneksi->prepare($sql);
+        $ps->execute([$id]);
+        $rs = $ps->fetch();
+        return $rs;
+    }
+
+    public function simpan($data){
+        $sql = "INSERT INTO pelanggan(kode,nama_pelanggan,jk,tmp_lahir,tgl_lahir,alamat,email,kartu_id) VALUES (?,?,?,?,?,?,?,?)";
+        $ps = $this->koneksi->prepare($sql);
+        $ps->execute($data);
+    }
+}
+
+
+?>
